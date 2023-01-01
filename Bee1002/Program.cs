c794a68ed30b32d42bd2a7bdc80e3f823980f079
isahonorato <isabelladdotcom@gmail.com>
2023-01-01 15:54:18 -0300

    first commit

---
+++ Bee1002/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bee1002
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double area, raio;
+            double n = 3.14159;
+            
+            Console.WriteLine("Digite o valor do raio");
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = Math.Pow(raio, 2) * n;
+
+            Console.WriteLine("A área do círculo é de " + area);
+
+            Console.ReadKey();
+            Console.ReadLine();
+        }
+    }
+}
