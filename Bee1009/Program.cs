1ea63ec6be0a42812905530da46278aebc9dab61
Isabella Honorato <isabellahonorato58@gmail.com>
2023-01-01 18:15:49 -0300

    Bee1009

---
+++ Bee1009/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bee1009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string vendedor;
+            double salarioFixo, totalVendas,salarioFinal;
+
+            Console.WriteLine("CÁLCULO DE COMISSÃO MENSAL - VENDEDORES");
+            Console.WriteLine("Insira o nome do vendedor");
+            vendedor = Console.ReadLine();
+            Console.WriteLine("Insira o salário padrão do vendedor:");
+            salarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Insira o valor total de vendas feitas no mês:");
+            totalVendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture) ;
+
+            salarioFinal = totalVendas * 15.0 / 100 + salarioFixo;
+
+            Console.WriteLine("O salário final do vendedor " + vendedor +  " é de R$" + salarioFinal.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
+            Console.ReadKey();
+            
+        }
+    }
+}
